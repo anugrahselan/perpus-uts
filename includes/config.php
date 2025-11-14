950a514548156b3f2a672ff7cc88ebbecb73d91b
Anugrah0603 <akbarakbar0319@gmail.com>
2025-11-14 12:59:19 +0700

    add confiig

--- includes/config.php
+++ includes/config.php
@@ -1,22 +1,13 @@
-<?php 
+<?php
 
-//Menentukan host, user, password, db_name sesuai dengan database yang dibuat
-
-// user pass database
 $db_host = 'localhost';
 $db_user = 'root';
 $db_pass = '';
 $db_name = 'db_perpus';
 
-//mematikan error reporting untuk mysqli
-//mysqli_report(MYSQLI_REPORT_OFF);
-
-//membuat koneksi
 $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
-//eror handling
 if ($mysqli->connect_error) {
-    echo "Gagal melakukan koneksi ke MySQL: " . $mysqli->connect_error;
-    exit();
+    die("koneksi gagal: " . $mysqli->connect_error);
 }
 ?>
