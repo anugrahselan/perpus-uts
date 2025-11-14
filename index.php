728f1ca9abfdd0a4f1f944ba765144bcf6bee277
Anugrah0603 <akbarakbar0319@gmail.com>
2025-11-14 12:58:56 +0700

    add index

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
-
-if(!isset($_SESSION['admin_logged_in'])) {
+if (!isset($_SESSION['admin_logged_in'])) {
     header("Location: login.php");
-    exit();
+    exit;
 }
+
+require_once "includes/config.php";
 
 define('MY_APP', true);
 
